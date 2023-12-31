628f50ff90ce97560d33d653b2e75e1be9692626
suzannefox <suzanne_fox@hotmail.com>
2023-12-31 23:07:59 +0000

    UPDATE : Add Crosstab functions

--- burrow.R
+++ burrow.R
@@ -1,6 +1,7 @@
 
 # ---------------------------------------------
 
+# information about a dataframe
 dfinfo <- function(df, diagnostics = FALSE, verbose = TRUE) {
 
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -65,7 +66,140 @@ dfinfo <- function(df, diagnostics = FALSE, verbose = TRUE) {
   return(dfout)
 }
 
-# get the alpha excel column designation 
+# -------------------------------------------------------------------------
+
+# Crosstabs : generate columns
+generate_xtab_column <- function(df_tab, col_item = '', options=c(), na_text = na_text) {
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # generates each column of the crosstab
+  # assumes df_tab is an input dataframe of 2 columns called sidevar, headvar
+  # if col_item == 'Make_Total' then generate a total column
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  text_total <- 'TOTAL'
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # total column for sidevar or headvar to form the framework of
+  # the table output. everything s joined to this, so 
+  # order and so on goes here
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  if (tolower(col_item) == 'make_total') {
+    line_tot <- df_tab %>% mutate(sidevar = text_total) %>% count(sidevar)
+    line_all <- df_tab %>% filter(!is.na(sidevar)) %>% count(sidevar)
+    line_nas <- df_tab %>% filter(is.na(sidevar)) %>% count(sidevar) %>% mutate(sidevar = na_text)
+    
+    # assumes at this point the sidevar can have any variable type
+    # need to sort first then change to character
+    # so that if sidevar is integer the sorting is correct
+    if ('sortside_n' %in% options) line_all <- line_all %>% arrange(desc(n))
+    line_all <- line_all %>% mutate(sidevar = as.character(sidevar))
+    
+    col_stub <- bind_rows(line_tot, line_all, line_nas) %>% rename(TOTAL = n)
+    return(col_stub)
+  }
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # this will generate a column of occurance counts for the
+  # sidevar for a specific item in the headvar denoted by col_item
+  # assumes at this point df_tab will be all character
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  df_tab <- df_tab %>% filter(headvar == col_item)
+  
+  # for each element of the sidevar
+  col_items <- bind_rows(
+    # generate a total
+    df_tab %>% 
+      summarise(n = n()) %>% 
+      mutate(sidevar = text_total) %>% 
+      select(sidevar, n),
+    
+    # totals for each side item
+    df_tab %>% 
+      group_by(sidevar) %>% 
+      summarise(n = n()) %>% 
+      ungroup() 
+  )
+  
+  return(col_items)
+}
+
+# Crosstabs : generate a crosstab
+xtab <- function(.data, side, header, options = c(), na_text = 'N/A') {
+  
+  df <- .data
+  
+  # STEP 0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # get the two variables involved in the cross tabulation
+  # so the code is generic make a dataframe with just those
+  # two variables, and call them sidevar and headvar
+  # rename these back to the original values before 
+  # returning the result
+  
+  df_tab <- df %>% 
+    select(sidevar = (!!enquo(side)),
+           headvar = (!!enquo(header)))
+  
+  # get string variables
+  side_txt = deparse(substitute(side))
+  head_txt = deparse(substitute(header))
+  
+  # get the type of variables
+  df_types <- sapply(df_tab, class)
+  
+  # STEP 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # create the stub/side/column 0 from the sidevar
+  # it has a total as the first element
+  # and if there are NAs those go at the end
+  col0 <- generate_xtab_column(df_tab, 
+                               col_item = 'Make_Total', 
+                               options = options,
+                               na_text = na_text) 
+  
+  # STEP 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # now that column 0 knows about any NAs, and has done
+  # the ordering appropriately for the variable type
+  # sidevar and headvar can be character and NAs can 
+  # be character with text as specified in na_text
+  df_tab <- df_tab %>% 
+    mutate(sidevar = as.character(sidevar)) %>% 
+    mutate(headvar = as.character(headvar)) %>% 
+    mutate(sidevar = coalesce(sidevar, na_text)) %>% 
+    mutate(headvar = coalesce(headvar, na_text))  
+  
+  # STEP 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # create an output column for each occurance in the headvar
+  head_items <- df_tab %>% count(headvar)
+  if ('sorthead_n' %in% options) head_items <- head_items %>% arrange(desc(n))
+  head_items <- head_items$headvar
+  if (na_text %in% head_items) head_items <- c(head_items[!head_items %in% c(na_text)], na_text)
+  
+  for (item in head_items) {
+    # make a column for each item
+    colx <- generate_xtab_column(df_tab,
+                                 col_item=item,
+                                 options = options,
+                                 na_text = na_text)
+    
+    out_name <- paste0(head_txt, '_', item)
+    colx <- colx %>% rename(!!out_name := n)
+    col0 <- col0 %>% full_join(colx, by = 'sidevar')
+  }
+  
+  # STEP 4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # rename sidevar to the original variable name
+  # set any nas to zero
+  col0 <- col0 %>% 
+    rename(!!side_txt := sidevar) %>% 
+    mutate(across(where(is.numeric), coalesce, 0)) %>% 
+    identity()
+  
+  return(col0)
+}
+
+# ---------------------------------------------
+# supporting functions
+
+# get the alpha excel column designation of a number
 excelcol <- function(number) {
   
   # 1 : more variables than permitted columns in excel
