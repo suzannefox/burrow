4b374fc2608ea0f8ae6c219f06eaa9864f8de820
suzannefox <suzanne_fox@hotmail.com>
2024-05-18 19:46:37 +0100

    UPDATES : Check for installed packages, move supporting functions into main

--- burrow.R
+++ burrow.R
@@ -1,10 +1,142 @@
 
-# ---------------------------------------------
-
 # information about a dataframe
 dfinfo <- function(df, diagnostics = FALSE, verbose = TRUE, catalog = FALSE) {
-
+  
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Ensure the required packages are available
+  if (!requireNamespace("glue", quietly = TRUE)) stop(" ... Package 'glue' needs to be available")
+  if (!requireNamespace("tidyverse", quietly = TRUE)) stop(" ... Package 'tidyverse' needs to be available")
+  library(tidyverse)
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # df <- .data
+  if (nrow(df) == 0 | ncol(df) == 0) {
+    print(glue::glue(' ... dataframe is empty'))
+    return()
+  }
+  
+  if (verbose) {
+    df_name <- deparse(substitute(df))
+    print(glue::glue(' ... dataframe {df_name} has {nrow(df)} records and {ncol(df)} variables'))
+  }
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # SUPPORTING FUNCTIONS
+  
+  # get the alpha excel column designation of a number
+  excelcol <- function(number) {
+    
+    # 1 : more variables than permitted columns in excel
+    if (number > 16384) return('16384 max')
+    if (number < 1) return('must be > 0')
+    
+    # 2 : find number of letters
+    letter_count = 0
+    if (number <= 26^1) {
+      letter_count <- 1
+    } else if (number <= 26^2) {
+      letter_count <- 2
+    } else if (number <= 26^3) {
+      letter_count <- 3 
+    } else {
+      return('some problem')
+    }
+    
+    # 3 : single letter columns
+    #     number will be in range 1 - 26, A is 65 in utf8
+    if (letter_count == 1) return(intToUtf8(64+number))
+    
+    # 4 : 2 letter columns
+    #     number will be in range 27 to 676
+    if (letter_count == 2) {
+      # first letter, how many 26's are there
+      num1 <- as.integer(number / 26)
+      # remainder, if 0 its the last in the set
+      rem1 <- number %% (num1 * 26)
+      
+      if (rem1 == 0) {
+        num1 <- num1 - 1
+        rem1 <- 26
+      }
+      
+      letter1 <- intToUtf8(64+num1)
+      letter2 <- intToUtf8(64+rem1)
+      return(paste0(letter1,letter2))
+    }
+    
+    # 5 : 3 letter columns
+    #     number will be in range 677 to 16384
+    if (letter_count == 3) {
+      # first letter, how many 26^2's are there
+      num1 <- as.integer(number / 26^2)
+      # remainder, if it's 0 it's the last in the set
+      rem1 <- number %% (num1 * 26^2)
+      
+      if (rem1 == 0) {
+        num1 <- num1 - 1
+        rem1 <- 676
+      }
+      
+      # second letter, how many 26's are there
+      # find whats left for the 26 x 26 outer matrix
+      number2 <- number - ((num1 * 26^2) - 26)
+      
+      num2 <- as.integer(number2 / 26)
+      # remainder, if 0 its the last in the set
+      rem2 <- number2 %% (num2 * 26)
+      print(number2)
+      
+      if (num2 == 0) {
+        num2 <- num2 - 1
+        rem2 <- 26
+      }
+      
+      letter1 <- intToUtf8(64+num1)
+      letter2 <- intToUtf8(64+num2)
+      letter3 <- intToUtf8(64+rem2)
+      return(paste0(letter1,letter2,letter3))
+    }
+    
+    # error
+    return('ERROR')
+  }
+  
+  # get means only for numeric columns
+  custom_mean <- function(variable) {
+    
+    varclass <- paste(class(variable), collapse = '.')
+    vartype <- paste(typeof(variable), collapse = '.')
+    
+    if (varclass %in% c('numeric') & vartype %in% c('double')) {
+      return_val <- mean(variable, na.rm = TRUE)
+      return(sprintf("%.1f", return_val))
+    } else {
+      return('-')
+    }
+  }
+  
+  # get skewness only for numeric columns
+  custom_skew <- function(variable) {
+    
+    varclass <- paste(class(variable), collapse = '.')
+    vartype <- paste(typeof(variable), collapse = '.')
+    
+    if (varclass %in% c('numeric') & vartype %in% c('double')) {
+      
+      n <- length(variable)
+      mean_x1 <- mean(variable)
+      sd_x1 <- sqrt(sum((variable - mean_x1)^2) / (n))
+      z1 <- (variable - mean_x1) / sd_x1
+      skewness <- sum(z1^3) / n
+      
+      return(sprintf("%.1f", skewness))
+    } else {
+      return('-')
+    }
+  }
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # MAIN BODY
   # 1.1 : get variable names
   dfout <- data.frame('Variables' = colnames(df), stringsAsFactors = FALSE) 
 
@@ -68,65 +200,74 @@ dfinfo <- function(df, diagnostics = FALSE, verbose = TRUE, catalog = FALSE) {
 
 # -------------------------------------------------------------------------
 
-# Crosstabs : generate columns
-generate_xtab_column <- function(df_tab, col_item = '', options=c(), na_text = na_text) {
-  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # generates each column of the crosstab
-  # assumes df_tab is an input dataframe of 2 columns called sidevar, headvar
-  # if col_item == 'Make_Total' then generate a total column
-  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  
-  text_total <- 'TOTAL'
-  
-  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # total column for sidevar or headvar to form the framework of
-  # the table output. everything s joined to this, so 
-  # order and so on goes here
-  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  if (tolower(col_item) == 'make_total') {
-    line_tot <- df_tab %>% mutate(sidevar = text_total) %>% count(sidevar)
-    line_all <- df_tab %>% filter(!is.na(sidevar)) %>% count(sidevar)
-    line_nas <- df_tab %>% filter(is.na(sidevar)) %>% count(sidevar) %>% mutate(sidevar = na_text)
-    
-    # assumes at this point the sidevar can have any variable type
-    # need to sort first then change to character
-    # so that if sidevar is integer the sorting is correct
-    if ('sortside_n' %in% options) line_all <- line_all %>% arrange(desc(n))
-    line_all <- line_all %>% mutate(sidevar = as.character(sidevar))
-    
-    col_stub <- bind_rows(line_tot, line_all, line_nas) %>% rename(TOTAL = n)
-    return(col_stub)
-  }
-  
-  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # this will generate a column of occurance counts for the
-  # sidevar for a specific item in the headvar denoted by col_item
-  # assumes at this point df_tab will be all character
-  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  df_tab <- df_tab %>% filter(headvar == col_item)
-  
-  # for each element of the sidevar
-  col_items <- bind_rows(
-    # generate a total
-    df_tab %>% 
-      summarise(n = n()) %>% 
-      mutate(sidevar = text_total) %>% 
-      select(sidevar, n),
-    
-    # totals for each side item
-    df_tab %>% 
-      group_by(sidevar) %>% 
-      summarise(n = n()) %>% 
-      ungroup() 
-  )
-  
-  return(col_items)
-}
-
 # Crosstabs : generate a crosstab
-xtab <- function(.data, side, header, options = c(), na_text = 'N/A') {
+xtab <- function(.data, side, header, options = c(), na_text = 'N/A', 
+                 sidecats = 0, headcats = 0, verbose = FALSE) {
   
   df <- .data
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # Ensure the required packages are available
+  if (!requireNamespace("glue", quietly = TRUE)) stop(" ... Package 'glue' needs to be available")
+  if (!requireNamespace("tidyverse", quietly = TRUE)) stop(" ... Package 'tidyverse' needs to be available")
+  library(tidyverse)
+  
+  # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  # SUPPORTING FUNCTIONS
+  # Crosstabs : generate columns
+  generate_xtab_column <- function(df_tab, col_item = '', options=c(), na_text = na_text) {
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # generates each column of the crosstab
+    # assumes df_tab is an input dataframe of 2 columns called sidevar, headvar
+    # if col_item == 'Make_Total' then generate a total column
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    text_total <- 'TOTAL'
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # total column for sidevar or headvar to form the framework of
+    # the table output. everything s joined to this, so 
+    # order and so on goes here
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    if (tolower(col_item) == 'make_total') {
+      line_tot <- df_tab %>% mutate(sidevar = text_total) %>% count(sidevar)
+      line_all <- df_tab %>% filter(!is.na(sidevar)) %>% count(sidevar)
+      line_nas <- df_tab %>% filter(is.na(sidevar)) %>% count(sidevar) %>% mutate(sidevar = na_text)
+      
+      # assumes at this point the sidevar can have any variable type
+      # need to sort first then change to character
+      # so that if sidevar is integer the sorting is correct
+      if ('sortside_n' %in% options) line_all <- line_all %>% arrange(desc(n))
+      line_all <- line_all %>% mutate(sidevar = as.character(sidevar))
+      
+      col_stub <- bind_rows(line_tot, line_all, line_nas) %>% rename(TOTAL = n)
+      return(col_stub)
+    }
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # this will generate a column of occurance counts for the
+    # sidevar for a specific item in the headvar denoted by col_item
+    # assumes at this point df_tab will be all character
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    df_tab <- df_tab %>% filter(headvar == col_item)
+    
+    # for each element of the sidevar
+    col_items <- bind_rows(
+      # generate a total
+      df_tab %>% 
+        summarise(n = n()) %>% 
+        mutate(sidevar = text_total) %>% 
+        select(sidevar, n),
+      
+      # totals for each side item
+      df_tab %>% 
+        group_by(sidevar) %>% 
+        summarise(n = n()) %>% 
+        ungroup() 
+    )
+    
+    return(col_items)
+  }
   
   # STEP 0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # get the two variables involved in the cross tabulation
@@ -145,6 +286,28 @@ xtab <- function(.data, side, header, options = c(), na_text = 'N/A') {
   
   # get the type of variables
   df_types <- sapply(df_tab, class)
+  
+  # if side is numeric, see if we want to categorise it
+  if (df_types[1] == "numeric" & sidecats > 0) {
+    if (verbose) print(glue::glue(' ... recoding {side_txt} from numberic to {sidecats} categories'))
+    df_tab <- df_tab %>% 
+      mutate(sidecat = as.character(cut(sidevar, breaks = sidecats))) %>% 
+      mutate(sidecat = str_replace(sidecat,'\\(','Between ')) %>% 
+      mutate(sidecat = str_replace(sidecat,',',' and ')) %>% 
+      mutate(sidecat = str_remove(sidecat,']')) %>% 
+      select(sidevar = sidecat, headvar)
+  }
+
+  # if head is numeric, see if we want to categorise it
+  if (df_types[2] == "numeric" & headcats > 0) {
+    if (verbose) print(glue::glue(' ... recoding {head_txt} from numberic to {headcats} categories'))
+    df_tab <- df_tab %>% 
+      mutate(headcat = as.character(cut(headvar, breaks = headcats))) %>% 
+      mutate(headcat = str_remove(headcat,'\\(')) %>% 
+      mutate(headcat = str_replace(headcat,',','-')) %>% 
+      mutate(headcat = str_remove(headcat,']')) %>% 
+      select(sidevar, headvar = headcat)
+  }
   
   # STEP 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # create the stub/side/column 0 from the sidevar
@@ -199,33 +362,35 @@ xtab <- function(.data, side, header, options = c(), na_text = 'N/A') {
 # -------------------------------------------------------------------------
 
 # count the contents of variables in a dataframe
-hcount <- function(dataframe, elem_max = 20, elem_sort = TRUE, col_include = c(), col_exclude = c()) {
+hcount <- function(.data, elem_max = 20, elem_sort = TRUE, cols_include = c(), cols_exclude = c()) {
   
+  df <- .data
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # determine columns to report on
   
   # if an include list is passed
-  count_include <- length(col_include)
+  count_include <- length(cols_include)
   
   # if an exclude list is passed
-  count_exclude <- length(col_exclude)
+  count_exclude <- length(cols_exclude)
   
   # get names
-  col_names <- dataframe %>% colnames()
-  count_names <- length(col_names)
-  
+  col_names <- df %>% colnames()
+  col_report <- col_names
+  count_report <- length(col_report)
+
   # an include takes precendence over an exclude
   # get names to report, and check include or exclude list is valid
   col_mode <- 'all'
   if (count_include > 0) {
     col_mode <- 'include'
-    col_report <- intersect(col_names, col_include)
+    col_report <- intersect(col_names, cols_include)
     count_report <- length(col_report)
     
     if (count_report != count_include) {
       print(' ... WARNING : Include list variables not found in data')
       
-      col_missing <- setdiff(col_include, col_names)
+      col_missing <- setdiff(cols_include, col_names)
       print(paste0(' ... Not Found : ', paste(col_missing, collapse = ',')))
       col_mode <- 'include WARNING'
     }
@@ -236,13 +401,13 @@ hcount <- function(dataframe, elem_max = 20, elem_sort = TRUE, col_include = c()
       print(' ... Include list takes precendence over exclude, ignoring')
     } else {
       col_mode <- 'exclude'
-      col_report <- setdiff(col_names, col_exclude)
+      col_report <- setdiff(col_names, cols_exclude)
       count_report <- length(col_report)
       
       if (count_report != (count_names - count_exclude)) {
         print(' ... WARNING : Include list variables not found in data')
         
-        col_missing <- setdiff(col_exclude, col_report)
+        col_missing <- setdiff(cols_exclude, col_report)
         print(paste0(' ... Not Found : ', paste(col_missing, collapse = ',')))
         col_mode <- 'exclude WARNING'
       }
@@ -268,7 +433,7 @@ hcount <- function(dataframe, elem_max = 20, elem_sort = TRUE, col_include = c()
     varname = myvars[i]
     
     # count occurances  
-    df_count <- dataframe %>%
+    df_count <- df %>%
       count(!!sym(varname), sort = elem_sort) %>%
       setNames(c('data', 'total')) %>%
       mutate(source = varname, .before = 1) %>%
@@ -295,7 +460,7 @@ hcount <- function(dataframe, elem_max = 20, elem_sort = TRUE, col_include = c()
     
     # calculate %ages
     df_count <- df_count %>%
-      mutate(pcent = paste0(round(total / nrow(dataframe) * 100), ' %'))
+      mutate(pcent = paste0(round(total / nrow(df) * 100), ' %'))
     
     df_count <- bind_rows(
       data.frame(source = varname,
@@ -314,120 +479,5 @@ hcount <- function(dataframe, elem_max = 20, elem_sort = TRUE, col_include = c()
   
   return(df_counts)
   
-}
-
-# ---------------------------------------------
-# supporting functions
-
-# get the alpha excel column designation of a number
-excelcol <- function(number) {
-  
-  # 1 : more variables than permitted columns in excel
-  if (number > 16384) return('16384 max')
-  if (number < 1) return('must be > 0')
-  
-  # 2 : find number of letters
-  letter_count = 0
-  if (number <= 26^1) {
-    letter_count <- 1
-  } else if (number <= 26^2) {
-    letter_count <- 2
-  } else if (number <= 26^3) {
-    letter_count <- 3 
-  } else {
-    return('some problem')
-  }
-  
-  # 3 : single letter columns
-  #     number will be in range 1 - 26, A is 65 in utf8
-  if (letter_count == 1) return(intToUtf8(64+number))
-  
-  # 4 : 2 letter columns
-  #     number will be in range 27 to 676
-  if (letter_count == 2) {
-    # first letter, how many 26's are there
-    num1 <- as.integer(number / 26)
-    # remainder, if 0 its the last in the set
-    rem1 <- number %% (num1 * 26)
-    
-    if (rem1 == 0) {
-      num1 <- num1 - 1
-      rem1 <- 26
-    }
-    
-    letter1 <- intToUtf8(64+num1)
-    letter2 <- intToUtf8(64+rem1)
-    return(paste0(letter1,letter2))
-  }
-  
-  # 5 : 3 letter columns
-  #     number will be in range 677 to 16384
-  if (letter_count == 3) {
-    # first letter, how many 26^2's are there
-    num1 <- as.integer(number / 26^2)
-    # remainder, if it's 0 it's the last in the set
-    rem1 <- number %% (num1 * 26^2)
-    
-    if (rem1 == 0) {
-      num1 <- num1 - 1
-      rem1 <- 676
-    }
-    
-    # second letter, how many 26's are there
-    # find whats left for the 26 x 26 outer matrix
-    number2 <- number - ((num1 * 26^2) - 26)
-    
-    num2 <- as.integer(number2 / 26)
-    # remainder, if 0 its the last in the set
-    rem2 <- number2 %% (num2 * 26)
-    print(number2)
-
-    if (num2 == 0) {
-      num2 <- num2 - 1
-      rem2 <- 26
-    }
-
-    letter1 <- intToUtf8(64+num1)
-    letter2 <- intToUtf8(64+num2)
-    letter3 <- intToUtf8(64+rem2)
-    return(paste0(letter1,letter2,letter3))
-  }
-  
-  # error
-  return('ERROR')
-}
-
-# get means only for numeric columns
-custom_mean <- function(variable) {
-  
-  varclass <- paste(class(variable), collapse = '.')
-  vartype <- paste(typeof(variable), collapse = '.')
-  
-  if (varclass %in% c('numeric') & vartype %in% c('double')) {
-    return_val <- mean(variable, na.rm = TRUE)
-    return(sprintf("%.1f", return_val))
-  } else {
-    return('-')
-  }
-}
-
-# get skewness only for numeric columns
-custom_skew <- function(variable) {
-  
-  varclass <- paste(class(variable), collapse = '.')
-  vartype <- paste(typeof(variable), collapse = '.')
-  
-  if (varclass %in% c('numeric') & vartype %in% c('double')) {
-    
-    n <- length(variable)
-    mean_x1 <- mean(variable)
-    sd_x1 <- sqrt(sum((variable - mean_x1)^2) / (n))
-    z1 <- (variable - mean_x1) / sd_x1
-    skewness <- sum(z1^3) / n
-    
-    return(sprintf("%.1f", skewness))
-  } else {
-    return('-')
-  }
 }
 
